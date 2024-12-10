e1a0a3884b45e5085976328d25e42d6aaefe62ba
Ileryadeh <ipdy@up.edu.ph>
2024-12-11 02:29:05 +0800

    Create categories.dart
    for item list

---
+++ aislequeue/lib/widgets/categories.dart
@@ -0,0 +1,23 @@
+// category_data.dart
+
+List<Map<String, String>> fishItems = [
+  {'name': 'Salmon Fillet (1kg)', 'price': '350'},
+  {'name': 'Tuna Steak (1kg)', 'price': '400'},
+  {'name': 'Sardines (1kg)', 'price': '150'},
+  {'name': 'Shrimp (500g)', 'price': '220'},
+  {'name': 'Mackerel (1kg)', 'price': '180'},
+];
+
+List<Map<String, String>> chickenItems = [
+  {'name': 'Whole Chicken (1kg)', 'price': '180'},
+  {'name': 'Chicken Wings (1kg)', 'price': '210'},
+  {'name': 'Chicken Breast (1kg)', 'price': '200'},
+  {'name': 'Chicken Drumsticks (1kg)', 'price': '220'},
+  {'name': 'Chicken Liver (1kg)', 'price': '120'},
+];
+
+List<Map<String, String>> otherItems = [
+  {'name': 'Baking Powder (500g)', 'price': '80'},
+  {'name': 'Vanilla Extract (50ml)', 'price': '150'},
+  // Add more items for the "Other" category
+];
